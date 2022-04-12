f6a0b289d7151eb1139c7c857cf5ec841ec89298
Dmitriy <dimryb@bk.ru>
2022-04-13 00:32:39 +0700

    Решение задачи №1 - Денежные переводы

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,12 @@
 fun main() {
+    val amount = 495112
+    val commission = calcCommission(amount)
+    println("Amount: ${amount/100}.${amount%100}₽, commission: ${commission/100}.${commission%100}₽")
+}
 
+fun calcCommission(amount: Int): Int{
+    val minCommissionKopecks = 3500
+    val commissionShare = 0.0075
+    return if (amount * commissionShare < minCommissionKopecks)
+        minCommissionKopecks else (amount * commissionShare).toInt()
 }
