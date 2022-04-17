dc75496eb703f2b970aa30d15c522c6518dcf0a2
Dmitriy <dimryb@bk.ru>
2022-04-17 19:35:24 +0700

    Исправил вывод рублей с копейками

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,18 @@
+const val ruble = 100
+
 fun main() {
     val amount = 495112
     val commission = calcCommission(amount)
-    println("Amount: ${amount/100}.${amount%100}₽, commission: ${commission/100}.${commission%100}₽")
+    println("Amount: ${rubleToString(amount)}, commission: ${rubleToString(commission)}")
 }
 
-fun calcCommission(amount: Int): Int{
-    val minCommissionKopecks = 3500
+fun calcCommission(amount: Int): Int {
+    val minCommission = 35 * ruble
     val commissionShare = 0.0075
-    return if (amount * commissionShare < minCommissionKopecks)
-        minCommissionKopecks else (amount * commissionShare).toInt()
+    return if (amount * commissionShare < minCommission)
+        minCommission else (amount * commissionShare).toInt()
+}
+
+fun rubleToString(amount: Int): String {
+    return String.format("%d.%02d₽", amount / ruble, amount % ruble)
 }
